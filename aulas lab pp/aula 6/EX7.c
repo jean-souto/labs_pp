f75d7df738d01171bb6c6f25267866b30bfe62ff
Jean Souto <jeansouto123@gmail.com>
2022-12-01 21:11:17 -0300

    Update EX7.c

--- aulas lab pp/aula 6/EX7.c
+++ aulas lab pp/aula 6/EX7.c
@@ -2,37 +2,31 @@
 são primos e suas respectivas posições no vetor*/
 #include <stdio.h>
 
-int main ()
+int main()
 {
-    int vetor [10];
-    int x, n, k, controle = 0;
+    int vetor[10];
+    int x, n, k, controle;
 
-    for (k = 0; k < 10; k++)
+
+    for(k = 0; k < 10; k++)
     {
-        printf ("Digite um valor: ");
-        scanf ("%d", &vetor[k]);
-
+        printf("Digite um valor: ");
+        scanf("%d", &vetor[k]);
     }
 
-    for (k = 0; k < 10; k++)
+    for(k = 0; k < 10; k++)
     {
-        for (x = 1; x < vetor[k]; x+=2)
+        for(n = 1, controle = 0; n <= vetor[k]; n++)
         {
-            for (n = 1; n <= x; n++)
+            if(vetor[k]%n == 0)
             {
-                if (x%n == 0)
-                {
-                    controle++;
-                }
+                controle++;
             }
+        }
 
-            if (controle == 2)
-            {
-                printf ("\n%d eh primo e sua posicao eh %d", vetor[k], k);
-            }
-
-            controle = 0;
-
+        if(controle == 2)
+        {
+            printf("\n%d eh primo e sua posicao eh %d", vetor[k], k);
         }
     }
 
